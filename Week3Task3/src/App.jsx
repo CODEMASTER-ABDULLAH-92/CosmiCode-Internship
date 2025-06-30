2776a5c4e5659c901a41b5455f897d37c262c34e
CODEMASTER-ABDULLAH-92 <abdullahpeerzada92@gmail.com>
2025-06-30 20:38:41 +0500

    Update App.jsx

--- Week3Task3/src/App.jsx
+++ Week3Task3/src/App.jsx
@@ -7,7 +7,8 @@ const [count,setCount] = useState(0);
 
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-indigo-100 to-white">
+    <div className="flex items-center justify-center flex-col  gap-5 min-h-screen bg-gradient-to-br from-indigo-100 to-white">
+      <h1 className="text-3xl">CosmiCode Counter App</h1>
       <Counter count={count} setCount={setCount} />
     </div>
   );
